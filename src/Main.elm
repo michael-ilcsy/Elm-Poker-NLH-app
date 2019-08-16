a64c04d44ba80a9bf95602475335cd0f6bde89cb
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-08-16 23:42:43 +0900

    feat: 一旦役のテキストを出すようにした

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, img, text)
+import Html exposing (Attribute, Html, button, div, h1, img, text)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 import Models.Card exposing (..)
 import Models.Deck as Deck exposing (..)
 import Poker.Holdem as Holdem exposing (..)
@@ -15,15 +16,29 @@ import Poker.Holdem as Holdem exposing (..)
 type alias Model =
     { board : Board
     , player1 : PlayerHand
+    , player1HandRankString : String
     , player2 : PlayerHand
+    , player2HandRankString : String
+    , gameStatus : GameStatus
+    , gameResult : Holdem.Result
     }
+
+
+type GameStatus
+    = BeforeInitialize
+    | Waiting
+    | Result
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { board = dummyBoard
       , player1 = dummyPlayerHand
+      , player1HandRankString = ""
       , player2 = dummyPlayerHand
+      , player2HandRankString = ""
+      , gameStatus = BeforeInitialize
+      , gameResult = Chop
       }
     , makeNewShuffledDeck InitDeck
     )
@@ -36,6 +51,7 @@ init =
 type Msg
     = InitDeck Deck
     | InitializeGame ( Board, PlayerHand, PlayerHand )
+    | SelectPlayer
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -49,6 +65,21 @@ update msg model =
                 | board = board
                 , player1 = playerHand1
                 , player2 = playerHand2
+                , gameStatus = Waiting
+              }
+            , Cmd.none
+            )
+
+        SelectPlayer ->
+            let
+                ( result, p1string, p2string ) =
+                    Holdem.judgeHandRank model.board model.player1 model.player2
+            in
+            ( { model
+                | player1HandRankString = p1string
+                , player2HandRankString = p2string
+                , gameStatus = Result
+                , gameResult = result
               }
             , Cmd.none
             )
@@ -60,23 +91,29 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] (model.board |> Holdem.mapBoard showCardImage)
-        , h1 [] [ text "どっちの勝ち？" ]
-        , div []
-            [ div [ class "player-hand" ]
-                (List.append
-                    (model.player1 |> Holdem.mapPlayerHand showCardImage)
-                    [ playerButton ]
-                )
-            , div [ class "chop" ] [ chopButton ]
-            , div [ class "player-hand" ]
-                (List.append
-                    (model.player2 |> Holdem.mapPlayerHand showCardImage)
-                    [ playerButton ]
-                )
+    if model.gameStatus /= BeforeInitialize then
+        div []
+            [ div [] (model.board |> Holdem.mapBoard showCardImage)
+            , h1 [] [ text "どっちの勝ち？" ]
+            , div []
+                [ div [ class "player-hand" ]
+                    (List.append
+                        (model.player1 |> Holdem.mapPlayerHand showCardImage)
+                        [ playerButton (onClick SelectPlayer) ]
+                        |> resultView model.gameStatus model.player1HandRankString
+                    )
+                , div [ class "chop" ] [ chopButton ]
+                , div [ class "player-hand" ]
+                    (List.append
+                        (model.player2 |> Holdem.mapPlayerHand showCardImage)
+                        [ playerButton (onClick SelectPlayer) ]
+                        |> resultView model.gameStatus model.player2HandRankString
+                    )
+                ]
             ]
-        ]
+
+    else
+        div [] []
 
 
 {-| トランプのカードの画像を表示します
@@ -86,12 +123,13 @@ showCardImage card =
     img [ src (card |> toImagePath) ] []
 
 
-playerButton : Html msg
-playerButton =
+playerButton : Attribute msg -> Html msg
+playerButton attr =
     button
         [ class "button"
         , class "is-link"
         , class "player-button"
+        , attr
         ]
         [ text "勝ち" ]
 
@@ -103,6 +141,18 @@ chopButton =
         , class "is-primary"
         ]
         [ text "引き分け" ]
+
+
+resultView : GameStatus -> String -> List (Html msg) -> List (Html msg)
+resultView gameStatus handRankString viewList =
+    List.append
+        viewList
+        (if gameStatus == Result then
+            [ div [] [ text handRankString ] ]
+
+         else
+            [ div [] [] ]
+        )
 
 
 
